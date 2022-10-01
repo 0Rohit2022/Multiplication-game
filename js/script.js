73b64b52fbd73dd3fcda33925957364ccba0c33a
Rohit kumar <112196544+0Rohit2022@users.noreply.github.com>
2022-10-01 21:35:03 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const num1 = Math.floor(Math.random()*10);
+const num2 = Math.floor(Math.random()*10);
+
+const questionEl = document.getElementById('question');
+
+const formEl = document.getElementById('form');
+
+const inputEl = document.getElementById('input');
+
+const scoreEl = document.getElementById('score');
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if(!score)
+{
+    score = 0;
+}
+
+
+questionEl.innerText = `What is ${num1} multiply by ${num2} ?`;
+
+scoreEl.innerText = `score : ${score}`
+
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener('submit',() => {
+    const userAns = +inputEl.value;
+    if(userAns === correctAns)
+    {
+        score++
+        updateLocalStorage()
+        
+    }
+    else{
+        score--
+        updateLocalStorage()
+        
+    }
+    
+})
+
+updateLocalStorage = () => {
+    localStorage.setItem('score',JSON.stringify(score));
+}
